fb2a32ed70f8e48313e827d6d0f81bd426dcd935
sorrelj <sorrelj@purdue.edu>
2023-05-13 20:00:33 -0500

    created nav and mobile nav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { NavigationComponent } from './shared/components/header/navigation/navigation.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { MobileNavigationComponent } from './shared/components/header/mobile-navigation/mobile-navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    NavigationComponent,
+    FooterComponent,
+    MobileNavigationComponent
   ],
   imports: [
     BrowserModule,
